999a71edc754984ba3561e306c63df0f67cc1bf4
Andrey Zhirkov <zhirkov.arty@gmail.com>
2023-07-19 22:50:49 +0300

    add def get_title in str_func.py

--- str_func.py
+++ str_func.py
@@ -1,2 +1,7 @@
 def get_string_up(string):
     return string.upper()
+
+
+def get_title(string):
+    """Функция возвращает заглавными первые буквы каждого слова в строке"""
+    return string.title()
